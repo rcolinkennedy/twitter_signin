ea5d41d4c2aea414793aa9eef574379bf2ccea5e
R. Colin Kennedy <c@rcolinkennedy.com>
2013-11-17 22:28:16 -0500

    Added nickname to User model to match Twitter

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118021533) do
+ActiveRecord::Schema.define(version: 20131118031026) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20131118021533) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
   end
 
 end
